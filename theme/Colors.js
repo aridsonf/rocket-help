495940d04fce46dc687cb4f3a6970d1d02bb3c24
Aridsonf <filhoaridson@gmail.com>
2023-08-23 19:30:16 -0300

    feat: styling default files

---
+++ theme/Colors.js
@@ -0,0 +1,22 @@
+export default {
+    product: {
+        primary: '#00875F',
+        secondary1: '#00B37E',
+        secondary2: '#04D361',
+    },
+    support: {
+        primary: '#996DFF',
+        secondary: '#FBA94C'
+    },
+    base: {
+        background: '#121214',
+        primaryShape: '#202024',
+        secondaryShape: '#29292E',
+        tertiaryShape: '#323238',
+        placeholder: '#7C7C8A',
+        textBase: '#C4C4CC',
+        title: '#E1E1E6',
+        white: '#FFFFFF',
+        error: '#F75A68'
+    }     
+};
---
+++ theme/Font.js
@@ -0,0 +1,11 @@
+export default {
+    heading: 'Roboto-Bold',
+    body: 'Roboto-Regular',
+    fontSize: {
+        xs: '12px',
+        sm: '14px',
+        md: '16px',
+        lg: '20px'
+    },
+    lineHeight: '32px'
+};
